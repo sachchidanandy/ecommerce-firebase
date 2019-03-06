065893681502195e19f62d8fc4efff8eab3b4339
sachchidanandY <sachchidanandy@mindfiresolutions.com>
2019-03-06 16:50:22 +0530

    Integrate Firebase with Redux and Firestore with Redux-Store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,29 @@
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/index';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import firebase from '../config/FirebaseConfig';
+
+//List of all store enhancers
+const enhancers = [
+    //Add getFirebase,getFirestore as extra argument so that we can access them in thunk functions
+    applyMiddleware(thunk.withExtraArgument({ getFirebase,getFirestore })),
+    //Add firebase config in reduxFirestore
+    reduxFirestore(firebase),
+    //Add firebase config in reactReduxFirebase
+    reactReduxFirebase(firebase)
+];
+
+//compose is used to combine all enhancers
+const composedEnhancers = compose(
+    ...enhancers
+);
 
 export default function(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        composedEnhancers
     );
 }
