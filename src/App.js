28e1aef22a7a94f79a96143a38ed7059d4564484
sachchidanandY <sachchidanandy@mindfiresolutions.com>
2019-03-08 12:28:13 +0530

    User Registration and Login Using Firebase Auth

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, {Component} from 'react';
 import AppRoutes from './Routes';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import { Redirect } from 'react-router-dom';
+import { firebaseAuth } from './config/FirebaseConfig';
 import './App.css';
-import {fetchUser} from './actions/userAction';
-import {Redirect} from 'react-router-dom';
 
 const store = configureStore();
 
 export default class App extends Component {
+
+    componentDidMount() {
+        firebaseAuth.onAuthStateChanged((user) => {
+            if (user) {
+                localStorage.setItem('user', JSON.stringify(user.uid));
+                return <Redirect to = '/dashboard'/>;
+            } else {
+                localStorage.removeItem('user');
+                return <Redirect to = '/'/>;
+            }
+        })
+    }
     render () {
         return (
             <Provider store = {store}>
--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,12 +1,9 @@
 import * as ActionType from '../constants/ActionType';
 import UserAPI from '../demoAPI/userAPI';
+import { firestore } from '../config/FirebaseConfig';
 
-function loginUserSuccess (user) {
-    return {type : ActionType.LOGIN_USER, user}
-}
-
-function registerUserSuccess (user) {
-    return {type : ActionType.LOGIN_USER, user}
+function fetchUserSuccess (user) {
+    return {type : ActionType.FETCH_USER, user}
 }
 
 function addToCardSuccess(cart) {
@@ -21,61 +18,45 @@ function buyProductsSuccess(lastOrder) {
     return {type : ActionType.BUY_PRODUCT, lastOrder}
 }
 
-export function fetchUser(email) {
+export function fetchUser(userId) {
     return function (dispatch) {
-        return UserAPI.fetchUser(email).then (user => {
-            dispatch(loginUserSuccess(user));
+        return firestore.collection('users').doc(`${userId}`).get()
+        .then (doc => {
+            return doc.data();
+        }).then( user => {
+            dispatch(fetchUserSuccess(user));
         }).catch ((error) => {
             throw error;
         }); 
     }
 }
-export function loginUser(user) {
-    return function (dispatch) {
-        return UserAPI.login(user).then (user => {
-            dispatch(loginUserSuccess(user));
-        }).catch ((error) => {
-            throw error;
-        });
-    };
-}
 
-export function registerUser(user) {
-    return function (dispatch) {
-        return UserAPI.saveUser(user).then (user => {
-            dispatch(registerUserSuccess(user));
-        }).catch ((error) => {
-            throw error;
-        });
-    };
-}
+// export function addToCart(userID, product) {
+//     return function (dispatch) {
+//         return UserAPI.addToCart(userID, product).then (cart => {
+//             dispatch(addToCardSuccess(cart));
+//         }).catch((error) => {
+//             throw error;
+//         });
+//     };
+// }
 
-export function addToCart(userID, product) {
-    return function (dispatch) {
-        return UserAPI.addToCart(userID, product).then (cart => {
-            dispatch(addToCardSuccess(cart));
-        }).catch((error) => {
-            throw error;
-        });
-    };
-}
+// export function deleteFromCart (userID, productSku) {
+//     return function (dispatch) {
+//         return UserAPI.deleteFromCart(userID, productSku).then (cart => {
+//             dispatch(deleteProductSuccess(cart));
+//         }).catch ((error) => {
+//             throw error;
+//         })
+//     }
+// }
 
-export function deleteFromCart (userID, productSku) {
-    return function (dispatch) {
-        return UserAPI.deleteFromCart(userID, productSku).then (cart => {
-            dispatch(deleteProductSuccess(cart));
-        }).catch ((error) => {
-            throw error;
-        })
-    }
-}
-
-export function buyProducts(userID, inCartProducts) {
-    return function (dispatch) {
-        return UserAPI.buyProducts(userID, inCartProducts).then ( lastOrder => {
-            dispatch(buyProductsSuccess(lastOrder));
-        }).catch (error => {
-            throw error;
-        })
-    }
-}
+// export function buyProducts(userID, inCartProducts) {
+//     return function (dispatch) {
+//         return UserAPI.buyProducts(userID, inCartProducts).then ( lastOrder => {
+//             dispatch(buyProductsSuccess(lastOrder));
+//         }).catch (error => {
+//             throw error;
+//         })
+//     }
+// }
